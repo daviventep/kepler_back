8017d6ac05dcae9e71931b71dd570dfcf18d9459
David Venté <davidventepolo@gmail.com>
2023-10-22 16:59:34 -0500

    22-Oct-2023 03 ~ Ajustes MainAdquisitionModel

--- src/main/java/com/kepler_apiweb/keplerapi/model/MainAdquisitionModel.java
+++ src/main/java/com/kepler_apiweb/keplerapi/model/MainAdquisitionModel.java
@@ -2,6 +2,8 @@ package com.kepler_apiweb.keplerapi.model;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import com.mongodb.DBObject;
@@ -9,18 +11,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document("MainAdquisitionModel")
+@Document("Main_Adquisition")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class MainAdquisitionModel {
     @Id
+    private String _id;
     private Double money_total_value;
     private int point_total_value;
     private Date creation_date;
     private Date delivery_date;
-    private String user_id;
+    private ObjectId user_id;
     private String status;
-    private List<DBObject> adquisiton_details = new ArrayList<>();
+    private List<AdquisitionDetail> adquisition_details = new ArrayList<>();
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class AdquisitionDetail {
+        private ObjectId product_id;
+        private Double money_unit_value;
+        private int point_unit_value;
+        private int quantity;
+        private String description;
+    }
 }
